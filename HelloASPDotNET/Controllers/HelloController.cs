ec2d49adca6f86d1ce650dce76fe88d267a4ac24
Sandhya Gurrala <g.sandhya2011@gmail.com>
2021-12-13 13:01:37 -0600

    update1

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloASPDotNET.Controllers
+{
+      
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Get: /hello/welcome
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")]
+
+        //public IActionResult Welcome(string name = "World")
+        //{
+        //    return Content("<h1>Welcome to my app," + name + "!</h1>", "text/html");
+        //}
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+        }
+        [Route("/hello/greeting")]
+        [HttpPost]
+        public IActionResult FormSubmission(string name, string language)
+        {
+            string greeting = CreateMessage(name, language);
+            return Content(greeting, "text/html");
+        }
+
+
+        public static string CreateMessage(string name, string lang)
+        {
+            if(lang == "english")
+            {
+                return $"Greetings, {name}!";
+            }
+            else if(lang == "french")
+            {
+                return $"Bonjour, {name}!";
+            }
+            else if(lang == "spanish")
+            {
+                return $"iHola, {name}!";
+            }
+            else if(lang == "hindi")
+            {
+                return $"Namaste, {name}!";
+            }
+            else if(lang == "german")
+            {
+                return $"Guten tag, {name}!!???";
+            }
+            return "No language selected!";
+        }
+    }
+}
